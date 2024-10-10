934df4624be2e0a7b6045e751de05729d3b8bddd
hi <138982320+hello-world-html@users.noreply.github.com>
2024-10-10 19:46:51 +0800

    one

---
+++ hu_com/script/script.js
@@ -0,0 +1,10 @@
+function tiaozhuan1() {
+    window.location.href = "./Unit/1/1.html";
+    alert("")
+}
+function tiaozhuan2(){
+    window.location.href = "./Unit/2/2.html";
+}
+function atiaozhuan1(){
+    window.location.href = "./Unit/test/test.html";
+}
